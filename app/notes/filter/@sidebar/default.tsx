fc5def299218cbf5eaddf7a8fe066f40b9be89e9
Dmytro-goit <dmytro.moiyseev@gmail.com>
2025-07-06 17:13:48 +0300

    Update default.tsx

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,4 +1,4 @@
-import SidebarNotes from "@/components/SideBarNotes/SidebarNotes";
+import SidebarNotes from "@/components/SidebarNotes/SidebarNotes";
 
 const arrayTags: string[] = ["Work", "Personal", "Meeting", "Shopping", "Todo"];
 
